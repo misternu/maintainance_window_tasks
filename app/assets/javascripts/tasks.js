4377de799eb7269e2a57ae47d2bd6bd69b41d0aa
Timothy Meixell <misternu@gmail.com>
2018-04-19 16:32:56 -0500

    finish tasks ajax

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,15 +1,50 @@
 document.addEventListener('DOMContentLoaded', function(event) {
+  const newTaskLink = document.getElementById('new_task');
   const taskList = document.getElementById('task_list');
-  taskList.addEventListener('click', event => {
-    if (event.target.classList.contains('checkbox')) {
-      checkTask(event.target.parentNode);
-    }
-  });
+  if (taskList) {
+    newTaskLink.addEventListener('click', newTask);
+    taskList.addEventListener('click', event => {
+      if (event.target.classList.contains('checkbox')) {
+        checkTask(event.target.parentNode);
+      } else if (event.target.classList.contains('delete')) {
+        deleteTask(event);
+      }
+    });
+    taskList.addEventListener('change', event => {
+      updateTask(event.target.parentNode);
+    });
+    taskList.addEventListener('submit', event => {
+      event.preventDefault();
+      updateTask(event.target);
+      event.target.querySelector('#task_title').blur();
+    });
+  }
 });
+
+const newTask = event => {
+  event.preventDefault();
+  const firstTask = document.getElementById('task_list').children[0];
+  ajax('POST', '/tasks', { title: 'Task' }, responseText => {
+    firstTask.insertAdjacentHTML('beforebegin', responseText);
+  });
+};
+
+const deleteTask = event => {
+  event.preventDefault();
+  event.stopPropagation();
+  const form = event.target.parentNode;
+  ajax('DELETE', form.action, {});
+  form.parentNode.hidden = true;
+};
+
+const updateTask = form => {
+  const newTitle = form.querySelector('#task_title').value;
+  ajax('PATCH', form.action, { task: { title: newTitle } });
+};
 
 const checkTask = form => {
   const newValue = toggleCheckbox(form);
-  ajax_send('PATCH', form.action, { task: { done: newValue } });
+  ajax('PATCH', form.action, { task: { done: newValue } });
 };
 
 const toggleCheckbox = form => {
@@ -26,9 +61,14 @@ const toggleCheckbox = form => {
   }
 };
 
-const ajax_send = (type, url, data) => {
+const ajax = (type, url, data, callback) => {
   const xhr = new XMLHttpRequest();
   xhr.open(type, url + '.json');
+  xhr.onload = function() {
+    if (callback && xhr.status === 200) {
+      callback(xhr.responseText);
+    }
+  };
   data = Object.assign(data, { authenticity_token: Rails.csrfToken() });
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify(data));
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all.sort_by(&:created_at)
+    @tasks = Task.all.order('created_at DESC')
   end
 
   def show; end
@@ -15,15 +15,20 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    if @task.save
-      redirect_to @task, notice: 'Task was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, notice: 'Task created.' }
+        format.json do
+          render partial: 'tasks/task.html.erb', locals: {task: @task}
+        end
+      else
+        format.html { render action: :new }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def update
-    @task = Task.find(params[:id])
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to @task, notice: 'Task updated.' }
@@ -37,7 +42,12 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+    respond_to do |format|
+      format.html do
+        redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+      end
+      format.json { head :ok }
+    end
   end
 
   private
